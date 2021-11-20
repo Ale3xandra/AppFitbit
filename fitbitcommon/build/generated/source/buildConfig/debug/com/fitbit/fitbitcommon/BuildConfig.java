d7abbd33539497c002718f0c00d22dfe8f275eb2
Ale3xandra <80722154+Ale3xandra@users.noreply.github.com>
2021-11-20 20:07:49 +0100

    Add files via upload

---
+++ fitbitcommon/build/generated/source/buildConfig/debug/com/fitbit/fitbitcommon/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fitbit.fitbitcommon;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.fitbit.fitbitcommon";
+  public static final String BUILD_TYPE = "debug";
+}
